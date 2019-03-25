24b4bf38d8f20dbf3ddea1247f3a1e06f9f316af
tmegh82 <40415982+tmegh82@users.noreply.github.com>
2019-03-26 02:01:34 +0530

    Rename insertion using recursion.cpp to DS_LA/insertion using recursion.cpp

--- DS_LA/insertion using recursion.cpp
+++ DS_LA/insertion using recursion.cpp
@@ -43,39 +43,39 @@ struct node *insertion(struct node *ptr , int key)
 
 struct node *deleteNode(struct node *root, int key)
 {
- 
-    if (root == NULL)
+Â 
+Â Â Â Â if (root == NULL)
      return root;
- 
-    if (key < root->key)
-        root->left = deleteNode(root->left, key);
+Â 
+Â Â Â Â if (key < root->key)
+Â Â Â Â Â Â Â Â root->left = deleteNode(root->left, key);
 
-    else if (key > root->key)
-        root->right = deleteNode(root->right, key);
- 
-    else
-    {
-        if (root->left == NULL)
-        {
-            struct node *temp = root->right;
-            free(root);
-            return temp;
-        }
-        else if (root->right == NULL)
-        {
-            struct node *temp = root->left;
-            free(root);
-            return temp;
-        }
-    struct node *current = root->right;
+Â Â Â Â else if (key > root->key)
+Â Â Â Â Â Â Â Â root->right = deleteNode(root->right, key);
+Â 
+Â Â Â Â else
+Â Â Â Â {
+Â Â Â Â Â Â Â Â if (root->left == NULL)
+Â Â Â Â Â Â Â Â {
+Â Â Â Â Â Â Â Â Â Â Â Â struct node *temp = root->right;
+Â Â Â Â Â Â Â Â Â Â Â Â free(root);
+Â Â Â Â Â Â Â Â Â Â Â Â return temp;
+Â Â Â Â Â Â Â Â }
+Â Â Â Â Â Â Â Â else if (root->right == NULL)
+Â Â Â Â Â Â Â Â {
+Â Â Â Â Â Â Â Â Â Â Â Â struct node *temp = root->left;
+Â Â Â Â Â Â Â Â Â Â Â Â free(root);
+Â Â Â Â Â Â Â Â Â Â Â Â return temp;
+Â Â Â Â Â Â Â Â }
+Â Â Â Â struct node *current = root->right;
 
-    while (current->left != NULL)
+Â Â Â Â while (current->left != NULL)
     {
-        current = current->left;
-        root->key = current->key;
-        root->right = deleteNode(root->right, current->key);
-    }
-    
+Â Â Â Â Â Â Â Â current = current->left;
+Â Â Â Â Â Â Â Â root->key = current->key;
+Â Â Â Â Â Â Â Â root->right = deleteNode(root->right, current->key);
+Â Â Â Â }
+Â Â Â Â 
 }
 return (root);
 }
